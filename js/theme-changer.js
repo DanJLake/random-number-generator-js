82bd758be17675d78fc88bcf72f987de7564c4b1
Dan <danjlake92@gmail.com>
2020-01-20 17:14:47 +0000

    Added initial text; added themes

---
+++ js/theme-changer.js
@@ -0,0 +1,13 @@
+function changeTheme(themeReq){
+    document.getElementsByTagName('body')[0].className = themeReq;
+
+    if (themeReq == 'vintage-wallpaper') {
+        document.getElementById('logo').src = './img/rng-white.png';
+    }
+    else if (themeReq == 'repeated-square-dark') {
+        document.getElementById('logo').src = './img/rng-green.png';
+    }
+    else if (themeReq == 'repeated-square' || themeReq == 'cork-board' || themeReq == 'blue-snow'){
+        document.getElementById('logo').src = './img/rng-black.png';
+    }
+}
